e0d77f0550a3337c48f620863902c9ad8907d732
anotherbondan <anandagautama3112@gmail.com>
2026-09-07 13:52:54 +0700

    fix: resolve type error

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,6 +14,7 @@ export const projectsData = [
       "/devtools/railway.svg",
     ],
     sourceCode: "https://github.com/anotherbondan/mata-kota",
+    projectUrl: "",
     category: "Competition",
   },
   {
